3d9e092666896dfa82993686d0d56d2fc94468c6
lidice <lidicedupuit@hotmail.com>
2024-03-06 12:40:42 -0300

    atualizaçao

--- src/components/ResultadoIMC/index.jsx
+++ src/components/ResultadoIMC/index.jsx
@@ -3,16 +3,17 @@ import styles from './resultadoIMC.module.css';
 
 const ResultadoIMC = ({ imc }) => {
     const classificacao = (imc) => {
-        if (imc < 18.5) return 'Abaixo do peso';
-        if (imc >= 18.5 && imc <= 24.9) return 'Peso normal';
-        if (imc >= 25 && imc <= 29.9) return 'Sobrepeso';
-        if (imc >= 30 && imc <= 34.9) return 'Obesidade grau 1';
-        if (imc >= 35 && imc <= 39.9) return 'Obesidade grau 2';
-        return 'Obesidade grau 3';
+      
+        if (imc < 18.5) return alert('Abaixo do peso');
+        else if (imc >= 18.5 && imc <= 24.9) return alert('Peso normal');
+        else if (imc >= 25 && imc <= 29.9) return alert('Sobrepeso');
+        else if (imc >= 30 && imc <= 34.9) return alert('Obesidade grau 1');
+        else if (imc >= 35 && imc <= 39.9) return alert('Obesidade grau 2');
+        else return alert('Obesidade grau 3');
     }
 
     return (
-        <div className={styles.resultado}>
+        <div className={styles.ResultadoIMC}>
             <p>Seu IMC é {imc.toFixed(1)}</p>
             <p>Classificação: {classificacao(imc)}</p>
         </div>
